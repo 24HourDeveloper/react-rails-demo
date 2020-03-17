26b51f38e64a9a8f6af72d6658ced8c70d9fa6c4
24hourdeveloper <mikeitt009@gmail.com>
2020-03-17 10:22:43 -0400

    installed react-router-dom and created an about and contact component and made routes for them.

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,12 +1,29 @@
 import React from "react"
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
 
 const App = () =>{
     return (
-        <div>
-            <h1>Hello World!</h1>
-            <p>Created by Michael Moore</p>
-        </div>
+        <>
+        <Router>
+        <nav>
+            <h1>NavBar</h1>
+            <ul>
+                <Link to="/about"><li>About</li></Link>
+                <Link to="/contact"><li>Contact</li></Link>
+            </ul>
+        </nav>
+        
+            <Switch>
+                <Route path="/about" component={About}/>
+                <Route path="/contact" component={Contact}/>
+            </Switch>
+        </Router>
+        </>
     )
 }
+
+const About = () => <div>About</div>
+
+const Contact = () => <div>Contact</div>
 
 export default App;
